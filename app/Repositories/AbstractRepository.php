376364f36d69c8437852b4dbef27346a2c20a23e
msousa <msousa.dev@outlook.com>
2022-08-06 21:23:23 -0300

    implementing abstract repository

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository {
+
+    public function __construct(Model $model) {
+        $this->model = $model;
+    }
+
+    public function selectAttributesRegisterRelated($attributes) {
+        $this->model = $this->model->with($attributes);
+    }
+
+    public function selectAttributes($attributes) {
+
+        $this->model = $this->model->selectRaw($attributes);
+    }
+
+    public function queryFilter($query) {
+        $query = explode(';', $query);
+
+        foreach($query as $key => $q) {
+
+            $querys = explode(':', $q);
+            $this->model = $this->model->where($querys[0], $querys[1], $querys[2]);
+
+        }
+    }
+
+    public function getResult() {
+        return $this->model->get();
+    }
+}
+
+?>
--- app/Repositories/CarBrandRepository.php
+++ app/Repositories/CarBrandRepository.php
@@ -1,39 +1,6 @@
 <?php
 namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
-
-
-class CarBrandRepository {
-
-    public function __construct(Model $model) {
-        $this->model = $model;
-    }
-
-    public function selectAttributesRegisterRelated($attributes) {
-        $this->model = $this->model->with($attributes);
-    }
-
-    public function selectAttributes($attributes) {
-
-        $this->model = $this->model->selectRaw($attributes);
-    }
-
-    public function queryFilter($query) {
-        $query = explode(';', $query);
-
-        foreach($query as $key => $q) {
-
-            $querys = explode(':', $q);
-            $this->model = $this->model->where($querys[0], $querys[1], $querys[2]);
-
-        }
-    }
-
-    public function getResult() {
-        return $this->model->get();
-    }
-
-}
+class CarBrandRepository extends AbstractRepository{}
 
 ?>
