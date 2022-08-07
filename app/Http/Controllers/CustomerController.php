dc540d26060ba50c56123feca131686d9a8a9061
msousa <msousa.dev@outlook.com>
2022-08-06 22:19:49 -0300

    Rest webservice API for customer resource

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,65 +2,116 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
+use App\Repositories\CustomerRepository;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
+    public function __construct(Customer $customer)
+    {
+        $this->customer = $customer;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $customerRepo = new CustomerRepository($this->customer);
+
+        if($request->has('query')){
+            $customerRepo->queryFilter($request->get('query'));
+        }
+
+        if($request->has('attributes')){
+            $customerRepo->SelectAttributes($request->get('attributes'));
+        } 
+
+        return response()->json($customerRepo->getResult(), 200);
     }
 
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCustomerRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreCustomerRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->customer->rules());
+
+        $customer = $this->customer->create([
+            'name' => $request->name
+        ]);
+
+        return response()->json($customer, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Customer  $customer
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Customer $customer)
+    public function show($id)
     {
-        //
+        $customer = $this->customer->with('model')->find($id);
+        if($customer === null) {
+            return response()->json(['error' => 'Não foi possível encontrar este carro!'], 404);
+        }
+
+        return response()->json($customer, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
-     * @param  \App\Models\Customer  $customer
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
+    public function update(Request $request, $id)
     {
-        //
+        $customer = $this->customer->find($id);
+        if($customer === null) {
+            return response()->json(['error' => 'Não foi possível atualizar esta marca!'], 404);
+        }
+        if($request->method() === 'PATCH') {
+            $dynamicRules = array();
+            //Percorre as regras definidas no Model
+            foreach($customer as $input => $rule) {
+                //Coleta apenas as regras aplicáveis nos parametros recebidos
+                if(array_key_exists($input, $request->all())){
+                    $dynamicRules[$input] = $rule;
+                }
+            }
+            $request->validate($dynamicRules);
+        } else {
+            $request->validate($customer->rules());
+        }
+
+        $customer->fill($request->all());
+        $customer->save();
+
+        return response()->json($customer, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Customer  $customer
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Customer $customer)
+    public function destroy($id)
     {
-        //
+        $customer = $this->customer->find($id);
+        if($customer === null) {
+            return response()->json(['error' => 'Não foi possível apagar esta marca!'], 404);
+        }
+
+        $customer->delete();
+
+        return response()->json(['msg' => 'Cliente deletado com sucesso!'], 200);
     }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
+
+    public function rules() {
+        return [
+            'name' => 'required',
+        ];
+    }
+
+    // public function car() {
+    //     return $this->belongsTo(CarModel::class);
+    // }
 }
